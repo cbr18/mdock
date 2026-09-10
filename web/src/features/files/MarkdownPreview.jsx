f9d3e5507d1b6af089b052e316b3e28b8ff8e8cf
Роман <cbr@MacBook-Air-Roman.local>
2026-09-10 10:09:30 +1000

    fix: enable GFM breaks:true for hard line breaks in preview

--- web/src/features/files/MarkdownPreview.jsx
+++ web/src/features/files/MarkdownPreview.jsx
@@ -2,10 +2,9 @@ import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
-import remarkBreaks from 'remark-breaks';
 import { useLanguage } from '../i18n/LanguageProvider.jsx';
 
-export const markdownRemarkPlugins = [remarkGfm, remarkFrontmatter, remarkBreaks];
+export const markdownRemarkPlugins = [[remarkGfm, { singleTilde: false, breaks: true }], remarkFrontmatter];
 export const markdownRehypePlugins = [rehypeHighlight];
 
 export function MarkdownPreview({ content }) {
